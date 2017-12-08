c517e7b1fa2ed98a2a8c1da6594b58f6f4ed33ba
Jon Hinson <jonhinsonfx@gmail.com>
2017-12-08 21:10:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,10 +3,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
-  name.each do |name|
+  names.each do |name|
   puts "Hello, my name is #{name}."
   end
 end
+
 
 def assign_rooms(attendees, room_assignments)
   badges.each do |name|
